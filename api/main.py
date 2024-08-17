d529e9ca7684be7e38ce71f8f9f8825537588a07
Bharath1910 <av.bharath@proton.me>
2024-08-17 14:56:56 +0530

    changed url

--- api/main.py
+++ api/main.py
@@ -15,4 +15,4 @@ def get_reinforcement(id):
     return f"Received request for reinforcement ID: {id} at {current_time}"
 
 if __name__ == '__main__':
-    app.run(port=80)
+    app.run(port=80, host="0.0.0.0")
